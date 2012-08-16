b2226dc35073e1484829f48dc431f789826bd343
benjamin <benjamin.michel.cn@gmail.com>
2012-08-16 09:02:53 -0500

    dynamic form with backbone view

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,7 +1,150 @@
 $(function () {
 
-	//Helpers
+	//definition of the record manager
+	var RecordManager = function (params) {
+		//the schema that we get from the server
+		var Schema = Backbone.Model.extend({
+			url: params.url
+		})
+		,	schemaUpdated = _.bind(function () {
+			_.bind(params.ready, this)();
+		}, this);
+
+		this.schema = new Schema();
+		this.schema.fetch().success(schemaUpdated);
+	};
+
+	//The functions associated with the record manager
 	
+	//CreateView
+	RecordManager.prototype.createView = function () { 
+		//the template for the create function
+		var template = Handlebars.compile(this.schema.get('template_create')) 
+		,	View = Backbone.View.extend({
+				template: template
+			,	events: {
+					'click #submit-button': 'submit'
+				}
+			,	render: function () {
+					console.log('render')
+					$(this.el).html(this.template());
+					return this;
+				}
+			,	submit: function () {
+					var form = document.forms[0]
+					,	newModel = {};
+					_.each(form, function (el) {
+						if (el.id !== 'submit-button') {
+							newModel[el.name] = el.value;
+						}
+					});
+					console.log('create a new model:');
+					console.log(newModel);
+					return false;
+				}
+		});
+    return new View({});
+	};
+
+	//DetailsView
+
+	//Make the instance of the views
+	var contactManager = new RecordManager({
+		url: '/contact-schema'
+	,	ready: function () {
+			console.log('ready');
+			$('#contact').html(this.createView().render().el);
+		}
+	});
+
+	
+	//adding the view to the page
+//	$('contact').html(contactManager.createView().render().el);
+
+
+	
+	/*
+	//Abstract Schema
+	//The schema of an abstract Model
+	//
+	//Public:
+	//	-getFormNew
+	//	-getList
+	//	-getDetails
+	//	-getFormEdit
+	//	-getFormSearch
+	//
+	//Private:
+	//	-getDefAttr
+	var AbstractSchema = BackBone.Model.extend({
+		getFormNew: function () {
+			var parser = Handlebars.compile(this.get('form_new'));
+			return parser(this.get('default_attr'));
+		}
+	,	getList: function (collection) {
+			var parser = Handlebars.compile(this.get('list'));
+			return parser(collection.toJSON());
+		}
+	,	getCollectionURL: function () { return this.get('collection_url') }
+	});
+	
+	//Contact Schema
+	//
+	var ContactSchema = AbstractSchema.extend({
+		url: '/contact-schema'
+	});
+
+	//Abstract Record Collection
+	//
+	//public:
+	//	-getSchema
+	//	-model
+	//	-RecordCollection
+	var RecordCollection = Backbone.Collection.extend({
+		initialize: function (schema) {
+			this.url = schema.getcollectionURL();
+			this.schema = schema;
+		}
+	,	getSchema: function () { return this.schema; }
+	});
+	
+
+
+	//Handlebars Helpers
+	
+	//text helper
+	Handlebars.registerHelper('form_new_input_text', function (input) {
+		return new Handlebars.SafeString(
+			'<label for="form-new-input-text-' + input.name + '">' + input.label + '</label>' +
+			'<input id="form-new-input-text-' + input.name
+				+ '" type="' + input.type
+				+ '" name="' + input.name
+				+ '" value="' + input.default_value + '" />'	
+		);
+	});
+	
+	//list helper
+	Handlebars.registerHelper('form_new_input_dropdown', function (input) {
+		var eachOption = function (list) {
+			return _.reduce(list, function (memo, item) {
+				return memo + '<option value="' + item.value + '">' + item.label + '</option>';
+			}, '');
+		};
+		return new Handlebars.SafeString(
+			'<label for="form-new-input-dropdown-' + input.name + '">' + input.label + '</label>' +
+			'<select id="form-new-input-dropdown-' + input.name + '">' +
+			eachOption(input.options) +
+			'</select>'
+		);
+	});
+
+    */
+
+
+
+
+
+	/*
 	//This helper just displays a link to a twitter account
 	Handlebars.registerHelper('twitter_link', function (link) {
 		return new Handlebars.SafeString(
@@ -38,5 +181,6 @@ $(function () {
 		document.getElementById('contact').innerHTML =
 			parser(contact.toJSON());
 	};
+	*/
 
 });
--- routes/index.js
+++ routes/index.js
@@ -21,15 +21,87 @@ exports.contact = function (req, res) {
 	});
 };
 
+var generateCreateTemplate = function (schema) {
+  return '<form>'
+		+	_.reduce(schema, function (memo, item) {
+
+			var html = [];
+			switch (item.type) {
+				case 'text':
+					html.push('<label for="input_' + item.name + '">');
+					html.push(item.label + '</label>');
+					html.push('<input id="input_' + item.name + '" ');
+					html.push('name="' + item.name + '" ');
+					html.push('type="text" value="' + item.value + '">');
+					html.push('</input>');
+					break;
+				case 'dropdown':
+					html.push('<label for="input_' + item.name + '">');
+					html.push(item.label + '</label>');
+					html.push('<select id="input_' + item.name + '" ');
+					html.push('name="' + item.name + '">');
+					_.each(item.options, function (opt) {
+						html.push('<option value="' + opt.value + '">');
+						html.push(opt.label + '</options>');
+					});
+					html.push('</select>');
+					break;
+				default:
+					console.log('Template generation warning:'
+						+ ' Unkown "type" in schema.item.type');
+			}
+			return memo + '<br/>' + html.join('');
+		}, '')
+		+ '<input id="submit-button" type="submit" value="Create"/>'
+		+ '</form>';
+};
+
 //This will return the schema of a contact.
 //In here, the contact only contains an HTML template
 exports.contactSchema = function (req, res) {
+
+	//in: json object
+	//out: html
+	var schema = {
+		create: [
+			{	name: 'full_name'
+			,	label: 'Full Name'
+			,	value: ''
+			,	type: 'text'
+			}
+		,	{	name: 'country'
+			,	label: 'Country'
+			,	options: [
+					{label: 'France', value: 'fr'}
+				,	{label: 'United Kingdom', value: 'uk'}
+				,	{label: 'Germany', value: 'de'}
+				,	{label: 'Finland', value: 'fi'}
+				,	{label: 'China', value: 'cn'}
+			]
+			,	type: 'dropdown'
+			}
+		]
+	};
+
+	//Generate the create template
+	var templateCreate = generateCreateTemplate(schema.create);
+
 	var htmlTemplate = '<p>My name is {{first_name}}.'
 									 + ' I am {{age}} years old, and'
 	 			 					 + ' I am a {{job}}!'
 									 + '{{twitter_link twitter}}';
+	/*
+	var templateCreate = '<form>'
+		+ '<label for="name">Give name</label>'
+		+ '<input id="name" name="username" type="text" value="def"/>'
+		+ '<select name="sex"><option value="m">Male</option>'
+			+ '<option value="f">Female</option>'
+		+ '<select>'
+		+	'<input id="submit-button" type="submit" value="ok"/>'
+		+ '</form>';
+	*/
 	res.json({
 		_id: '1'
-	,	template: htmlTemplate
+	,	template_create: templateCreate
 	});
 };
